b2973f4d1099a46418743f0951f306f3eefe36d9
SS <66886825+EarlMilktea@users.noreply.github.com>
2024-06-07 13:38:01 +0900

    :white_check_mark: Add tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -11,6 +11,60 @@ def test_invalidtype() -> None:
         _ = ErrorBracket.parse(1)  # type: ignore[arg-type]
 
 
+def test_ng_nobrac() -> None:
+    """Provide a string without brackets."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100")
+
+
+def test_ng_halfbrac() -> None:
+    """Provide a string with broken bracket."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100(3")
+
+
+def test_ng_emptybrac() -> None:
+    """Provide a string with empty brackets."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100()")
+
+
+def test_ng_invbrac() -> None:
+    """Provide a string with inverted brackets."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100)3(")
+
+
+def test_ng_manybrac() -> None:
+    """Provide a string with multiple brackets."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100(2)(3)")
+
+
+def test_ng_nestbrac() -> None:
+    """Provide a string with nested brackets."""
+    with pytest.raises(ValueError, match=r".* does not have proper bracket."):
+        _ = ErrorBracket.parse("100(2(3))")
+
+
+def test_ng_invalidval() -> None:
+    """Provide a string with invalid characters for val."""
+    with pytest.raises(ValueError, match="Cannot construct Decimal from '10a'."):
+        _ = ErrorBracket.parse("10a(2)")
+
+
+def test_ng_invaliderr() -> None:
+    """Provide a string with invalid characters for err."""
+    with pytest.raises(ValueError, match="Cannot construct Decimal from '2a'."):
+        _ = ErrorBracket.parse("10(2a)")
+
+
+def test_ng_longerr() -> None:
+    """Provide a string with too long err."""
+    with pytest.raises(ValueError, match=r".* has too many digits inside the bracket."):
+        _ = ErrorBracket.parse("100(2000000000)")
+
+
 @pytest.mark.parametrize(
     ("ex", "ref"),
     [
