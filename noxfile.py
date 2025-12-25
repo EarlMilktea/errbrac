f14fb91f1f65955d437aa971830f265129fcda3b
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-12-26 05:37:06 +0900

    :wrench: Use editable installs

--- noxfile.py
+++ noxfile.py
@@ -6,5 +6,5 @@ from nox import Session
 
 @nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session: Session) -> None:
-    session.install("pytest")
+    session.install("-e", ".[dev]")
     session.run("pytest")
