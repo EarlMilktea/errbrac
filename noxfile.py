b2a3d8a045d0fff9934f38ce245af871ad1091fe
SS <66886825+EarlMilktea@users.noreply.github.com>
2025-12-26 05:14:37 +0900

    :construction: Migrate to nox

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import nox
+from nox import Session
+
+
+@nox.session(python=["3.10", "3.11", "3.12", "3.13", "3.14"])
+def tests(session: Session) -> None:
+    session.install("pytest")
+    session.run("pytest")
